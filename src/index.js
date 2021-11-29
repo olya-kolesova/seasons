e724478700840633f0d59b09b413e983f07b4542
Olga Kolesova <olka.kolesova@gmail.com>
2021-11-29 22:38:18 +0000

    set state

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
   class App extends React.Component {
-    render() {
+    constructor(props) {
+      super(props);
+
+      this.state = { lat: null};
+
       window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
+        position => {
+          this.setState({ lat: position.coords.latitude })
+        },
         err => console.log(err)
       );
+    };
+
+    render() {
 
       return (
-        <div>Latitude: </div>
+        <div>Latitude: {this.state.lat} </div>
       );
     }
   }
