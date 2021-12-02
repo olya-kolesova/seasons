5fb29ed0f8c4d62b074036240dd7c32eb529b20f
Olga Kolesova <olka.kolesova@gmail.com>
2021-12-02 19:44:49 +0000

    added DidMount component

--- src/index.js
+++ src/index.js
@@ -7,15 +7,16 @@ import ReactDOM from 'react-dom';
 
       this.state = { lat: null, errorMessage: ''};
 
+    };
+
+    componentDidMount() {
       window.navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({ lat: position.coords.latitude })
-        },
+        position => this.setState({ lat: position.coords.latitude }),
         err => {
           this.setState({ errorMessage: err.message })
         }
       );
-    };
+    }
 
     render() {
       if (this.state.errorMessage && !this.state.lat) {
